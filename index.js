cf184c282d4c3bffc8fbf5cdff2628c476597c89
rainnier <rainnier@gmail.com>
2022-09-19 11:37:14 +0800

    with wheel

--- index.js
+++ index.js
@@ -1,8 +1,127 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.testFunction = void 0;
+exports.checkCondition = exports.loopPlane = exports.displayWheels = exports.addWheel = exports.addWheels = exports.addPlane = exports.Days = exports.testFunction = void 0;
 const testFunction = () => {
     console.log("Hello to Typescript");
 };
 exports.testFunction = testFunction;
+var parent;
+parent = "myString";
+parent = 500;
+var nameArray = [];
+var otherArray = new Array(10);
+nameArray.push("myName");
+otherArray.push("newName");
+var Days;
+(function (Days) {
+    Days[Days["Monday"] = 0] = "Monday";
+    Days[Days["Tuesday"] = 1] = "Tuesday";
+    Days[Days["Wednesday"] = 2] = "Wednesday";
+    Days[Days["Thursday"] = 3] = "Thursday";
+    Days[Days["Friday"] = 4] = "Friday";
+    Days[Days["Saturday"] = 5] = "Saturday";
+    Days[Days["Sunday"] = 6] = "Sunday";
+})(Days = exports.Days || (exports.Days = {}));
+function addPlane2(data) {
+}
+var planeList = [];
+const addPlane = (data) => {
+    planeList.push(data);
+};
+exports.addPlane = addPlane;
+var myWheels = [];
+const addWheels = (...wheelsArray) => {
+    wheelsArray.forEach((item) => {
+        console.log(item);
+    });
+};
+exports.addWheels = addWheels;
+const addWheel = (wheel) => {
+    myWheels.push(wheel);
+    (0, exports.displayWheels)();
+};
+exports.addWheel = addWheel;
+const displayWheels = () => {
+    let list = document.getElementById('wheelList');
+    if (list) {
+        while (list.hasChildNodes() && list.firstChild) {
+            list.removeChild(list.firstChild);
+        }
+    }
+    myWheels.forEach((item) => {
+        if (list) {
+            let li = document.createElement("li");
+            li.innerText = item;
+            list.appendChild(li);
+        }
+    });
+};
+exports.displayWheels = displayWheels;
+//addWheels("small wheel", "medium wheel", "large wheel");
+const loopPlane = () => {
+    // forEach
+    planeList.forEach((item) => {
+        console.log("forEach");
+        console.log(item);
+    });
+    // map
+    planeList.map((item) => {
+        console.log("map");
+        console.log(item);
+    });
+    // for loop
+    for (let i = 0; i < planeList.length; i++) {
+        console.log("for loop");
+        console.log(planeList[i]);
+    }
+    // while loop and do while
+    /*  let i = 0;
+     while(i < planeList.length) {
+         console.log("while loop")
+         console.log(planeList[i])
+         i++
+     } */
+    // enhanced for loop
+    for (let plane of planeList) {
+        console.log("enhanced for loop");
+        console.log(plane);
+    }
+    // index
+    for (let ind in planeList) {
+        console.log("index loop");
+        console.log(planeList[ind]);
+    }
+};
+exports.loopPlane = loopPlane;
+const checkCondition = () => {
+    var condition = true;
+    var s = "first";
+    if (condition) {
+    }
+    else if (!condition) {
+    }
+    else {
+    }
+    switch (s) {
+        case "first":
+            break;
+        case "second":
+            break;
+        default:
+    }
+};
+exports.checkCondition = checkCondition;
+var tuple = ["myName", 1];
+var tuple2 = ["first", 1, 2];
+var plane1 = {
+    name: "JetLee",
+    model: "FighterJet"
+};
+var plane2 = {
+    name: "Bruce Lee",
+    model: "Jumbo Jet"
+};
+(0, exports.addPlane)(plane1);
+(0, exports.addPlane)(plane2);
+(0, exports.loopPlane)();
 (0, exports.testFunction)();
--- index.ts
+++ index.ts
@@ -2,4 +2,138 @@ export const testFunction = () => {
     console.log("Hello to Typescript")
 }
 
+var parent:any;
+parent = "myString";
+parent = 500;
+
+var nameArray:string[] = [];
+var otherArray = new Array(10);
+
+nameArray.push("myName");
+otherArray.push("newName")
+
+export enum Days {
+    Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+}
+
+interface Plane {
+    name:string
+    model:string
+    fly?: (destination:string) => string
+}
+
+function addPlane2(data:Plane) {
+}
+
+var planeList:Plane[] = [];
+export const addPlane = (data:Plane) => {
+    planeList.push(data)
+}
+
+var myWheels:string[] = [];
+export const addWheels = (...wheelsArray:string[]) => {
+    wheelsArray.forEach((item) => {
+        console.log(item)
+    })
+}
+
+export const addWheel = (wheel:string) => {
+    myWheels.push(wheel);
+    displayWheels();
+}
+
+export const displayWheels = () => {
+    let list = document.getElementById('wheelList');
+    if(list) {
+        while(list.hasChildNodes() && list.firstChild) {
+            list.removeChild(list.firstChild)
+        }
+    }
+    myWheels.forEach((item) => {
+        if(list) {
+            let li = document.createElement("li");
+            li.innerText = item
+            list.appendChild(li)
+        }
+    })
+}
+
+//addWheels("small wheel", "medium wheel", "large wheel");
+
+export const loopPlane = () => {
+    // forEach
+    planeList.forEach((item) => {
+        console.log("forEach")
+        console.log(item)
+    })
+    
+    // map
+    planeList.map((item) => {
+        console.log("map")
+        console.log(item)
+    })
+    
+    // for loop
+    for(let i = 0; i < planeList.length; i++) {
+        console.log("for loop")
+        console.log(planeList[i])
+    }
+    
+    // while loop and do while
+   /*  let i = 0;
+    while(i < planeList.length) {
+        console.log("while loop")
+        console.log(planeList[i])
+        i++
+    } */
+    
+    // enhanced for loop
+    for(let plane of planeList) {
+        console.log("enhanced for loop")
+        console.log(plane)
+    }
+    
+    // index
+    for(let ind in planeList) {
+        console.log("index loop")
+        console.log(planeList[ind])
+    }
+}
+
+export const checkCondition = () => {
+    var condition:boolean = true;
+    var s:string = "first"
+    if(condition) {
+
+    } else if(!condition) {
+
+    } else {
+
+    }
+
+    switch(s) {
+        case "first": 
+        break;
+        case "second":
+            break;
+        default: 
+    }
+}
+
+var tuple = ["myName", 1];
+var tuple2:(string | number)[] = ["first", 1, 2];
+
+var plane1:Plane = {
+    name: "JetLee",
+    model: "FighterJet"
+}
+
+var plane2:Plane = {
+    name: "Bruce Lee",
+    model: "Jumbo Jet"
+}
+addPlane(plane1);
+addPlane(plane2);
+
+loopPlane();
 testFunction();
